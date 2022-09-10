bb94f53feccd1d45721a3ab721bf4ade323dec53
julian chadwick <noster91@gmail.com>
2022-09-10 14:55:47 -0300

    Boostrapeadas las views de Task

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ('/')
+  resources :tasks
   # root 'articles#index'
-  get '/tasks', to: 'tasks#index'
-  post '/tasks', to: 'tasks#create'
-  get '/task/new', to: 'tasks#new', as: 'new_task'
-  get '/task/:id', to: 'tasks#show', as: 'task'
-  get '/task/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch '/task/:id', to: 'tasks#update'
-  delete '/task/:id', to: 'tasks#destroy'
+  # get '/tasks', to: 'tasks#index'
+  # post '/tasks', to: 'tasks#create'
+  # get '/task/new', to: 'tasks#new', as: 'new_task'
+  # get '/task/:id', to: 'tasks#show', as: 'task'
+  # get '/task/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch '/task/:id', to: 'tasks#update'
+  # delete '/task/:id', to: 'tasks#destroy'
 end
