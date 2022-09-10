98a2bb4e0cde95a19875a7762a2fa5f735ede924
julian chadwick <noster91@gmail.com>
2022-09-10 13:40:30 -0300

    CRUD creado

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ('/')
   # root 'articles#index'
   get '/tasks', to: 'tasks#index'
-  get '/task/new', to: 'task#new'
-  get '/task/:id', to: 'task#show'
+  post '/tasks', to: 'tasks#create'
+  get '/task/new', to: 'tasks#new', as: 'new_task'
+  get '/task/:id', to: 'tasks#show', as: 'task'
+  get '/task/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/task/:id', to: 'tasks#update'
+  delete '/task/:id', to: 'tasks#destroy'
 end
